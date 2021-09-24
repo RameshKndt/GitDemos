e5b89dc0e467b5c0c3d3b1f643ba0e2069f68bf3
RameshKndt <kndtjavaj2ee@gmail.com>
2021-09-24 14:11:42 +0530

    changed

--- src/com/ibm/gitdemo/GitTest.java
+++ src/com/ibm/gitdemo/GitTest.java
@@ -2,6 +2,6 @@ package com.ibm.gitdemo;
 
 public class GitTest {
 	 public static void main(String[] args) {
-		System.out.println("Git TESTingG.....");
+		System.out.println("Git TESTingG.....oadofoadofoad");
 	}
 }
